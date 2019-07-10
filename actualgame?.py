cdd61be51f4c360056bedcae0a634087a03b1884
Danny <daniel.zakharov1@ctyi.org>
2019-07-10 09:26:27 +0100

    made str card names into images

--- actualgame?.py
+++ actualgame?.py
@@ -8,12 +8,14 @@ class Card(pygame.sprite.Sprite):
     cardAttack = 0
     cardHealth = 0
     cardEffect = 0
+    cardPosition = 0
     highlighted = False
     def cardAlive(self):
         if cardHealth <= 0:
             return False
     def __init__(self, IMAGE):
         pygame.sprite.Sprite.__init__(self)
+    
         self.image = IMAGE
         self.rect = self.image.get_rect()
         
@@ -23,22 +25,6 @@ class Player:
         self.rect = self.image.get_rect(center=(self.cardPosition[0]+100, self.cardPosition[1]+125))
     currentMana = 0
     health = 30
-
-class Card(pygame.sprite.Sprite):
-    """The card class that will be used to give every card an ID""" 
-    cardID = 0
-    cardCost = 0
-    cardAttack = 0
-    cardHealth = 0
-    cardEffect = 0
-    highlighted = False
-    cardPosition = 0
-    def cardAlive(self):
-        if cardHealth <= 0:
-            return False
-    def __init__(self, IMAGE):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = IMAGE
 
 hand1 = (50, 745)
 hand2 = (266, 746)
@@ -62,128 +48,128 @@ board12 = (909, 129)
 board13 = (1123, 130)
 board14 = (1336, 129)
 
-card1 = Card('2mana23.png')
+card1 = Card(pygame.image.load('2mana23.png'))
 card1.cardID = 1
 card1.cardCost = 2
 card1.cardAttack = 2
 card1.cardHealth = 3
 
-card2 = Card('2mana23.png')
+card2 = Card(pygame.image.load('2mana23.png'))
 card2.cardID = 1
 card2.cardCost = 2
 card2.cardAttack = 2
 card2.cardHealth = 3
 
-card3 = Card('2mana23.png')
+card3 = Card(pygame.image.load('2mana23.png'))
 card3.cardID = 1
 card3.cardCost = 2
 card3.cardAttack = 2
 card3.cardHealth = 3
 
-card4 = Card('2mana23.png')
+card4 = Card(pygame.image.load('2mana23.png'))
 card4.cardID = 1
 card4.cardCost = 2
 card4.cardAttack = 2
 card4.cardHealth = 3
 
-card5 = Card('2mana23.png')
+card5 = Card(pygame.image.load('2mana23.png'))
 card5.cardID = 1
 card5.cardCost = 2
 card5.cardAttack = 2
 card5.cardHealth = 3
 
-card6 = Card('2mana23.png')
+card6 = Card(pygame.image.load('2mana23.png'))
 card6.cardID = 1
 card6.cardCost = 2
 card6.cardAttack = 2
 card6.cardHealth = 3
 
-card7 = Card('2mana23.png')
+card7 = Card(pygame.image.load('2mana23.png'))
 card7.cardID = 1
 card7.cardCost = 2
 card7.cardAttack = 2
 card7.cardHealth = 3
 
-card8 = Card('2mana23.png')
+card8 = Card(pygame.image.load('2mana23.png'))
 card8.cardID = 1
 card8.cardCost = 2
 card8.cardAttack = 2
 card8.cardHealth = 3
 
-card9 = Card('2mana23.png')
+card9 = Card(pygame.image.load('2mana23.png'))
 card9.cardID = 1
 card9.cardCost = 2
 card9.cardAttack = 2
 card9.cardHealth = 3
 
-card10 = Card('2mana23.png')
+card10 = Card(pygame.image.load('2mana23.png'))
 card10.cardID = 1
 card10.cardCost = 2
 card10.cardAttack = 2
 card10.cardHealth = 3
 
-card11 = Card('2mana23.png')
+card11 = Card(pygame.image.load('2mana23.png'))
 card11.cardID = 1
 card11.cardCost = 2
 card11.cardAttack = 2
 card11.cardHealth = 3
 
-card12 = Card('2mana23.png')
+card12 = Card(pygame.image.load('2mana23.png'))
 card12.cardID = 1
 card12.cardCost = 2
 card12.cardAttack = 2
 card12.cardHealth = 3
 
-card13 = Card('2mana23.png')
+card13 = Card(pygame.image.load('2mana23.png'))
 card13.cardID = 1
 card13.cardCost = 2
 card13.cardAttack = 2
 card13.cardHealth = 3
 
-card14 = Card('2mana23.png')
+card14 = Card(pygame.image.load('2mana23.png'))
 card14.cardID = 1
 card14.cardCost = 2
 card14.cardAttack = 2
 card14.cardHealth = 3
 
-card15 = Card('2mana23.png')
+card15 = Card(pygame.image.load('2mana23.png'))
 card15.cardID = 1
 card15.cardCost = 2
 card15.cardAttack = 2
 card15.cardHealth = 3
 
-card16 = Card('2mana23.png')
+card16 = Card(pygame.image.load('2mana23.png'))
 card16.cardID = 1
 card16.cardCost = 2
 card16.cardAttack = 2
 card16.cardHealth = 3
 
-card17 = Card('2mana23.png')
+card17 = Card(pygame.image.load('2mana23.png'))
 card17.cardID = 1
 card17.cardCost = 2
 card17.cardAttack = 2
 card17.cardHealth = 3
 
-card18 = Card('2mana23.png')
+card18 = Card(pygame.image.load('2mana23.png'))
 card18.cardID = 1
 card18.cardCost = 2
 card18.cardAttack = 2
 card18.cardHealth = 3
 
-card19 = Card('2mana23.png')
+card19 = Card(pygame.image.load('2mana23.png'))
 card19.cardID = 1
 card19.cardCost = 2
 card19.cardAttack = 2
 card19.cardHealth = 3
 
-card20 = Card('2mana23.png')
+card20 = Card(pygame.image.load('2mana23.png'))
 card20.cardID = 1
 card20.cardCost = 2
 card20.cardAttack = 2
 card20.cardHealth = 3
 
 
-nullcard = Card('2mana23.png')
+nullcard = Card(pygame.image.load('2mana23.png'))
 selected = nullcard 
 highlight = pygame.image.load("HIGHLIGHT.png")
 highlighted = nullcard
@@ -287,7 +273,7 @@ while True :
                         display_surface.blit(highlight, c.cardPosition)
                         selected.highlighted = False
                         c.highlighted=not c.highlighted
-                        selected = c
+                        selected = c        
             
 
     # checks for quit
